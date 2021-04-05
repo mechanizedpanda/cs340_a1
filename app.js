4ef6564802f95d95142e6d6149eb20d00d1f0533
mechanizedpanda <54003040+mechanizedpanda@users.noreply.github.com>
2021-04-05 03:05:51 -0400

    should be fixed now

--- app.js
+++ app.js
@@ -5,41 +5,67 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 PORT        = 9912;                 // Set a port number at the top so it's easy to change in the future
 var db = require('./database/db-connector')  // Database
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', exphbs({                     // Create an instance of the handlebars engine to process templates
+    extname: ".hbs"                                  // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+}));
+app.set('view engine', '.hbs');
+
 
 /*
     ROUTES
 */
 app.get('/', function(req, res)
 {
-    // Define our queries
-    query1 = 'DROP TABLE IF EXISTS diagnostic;';
-    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-    query4 = 'SELECT * FROM diagnostic;';
+    let query1 = "SELECT * FROM bsg_people;";               // Define our query
 
-    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+    db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-    // DROP TABLE...
-    db.pool.query(query1, function (err, results, fields){
+        res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+    })                                                      // an object where 'data' is equal to the 'rows' we
+});
 
-        // CREATE TABLE...
-        db.pool.query(query2, function(err, results, fields){
+app.post('/add-person-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
 
-            // INSERT INTO...
-            db.pool.query(query3, function(err, results, fields){
+    // Capture NULL values
+    let homeworld = parseInt(data['input-homeworld']);
+    if (isNaN(homeworld))
+    {
+        homeworld = 'NULL'
+    }
 
-                // SELECT *...
-                db.pool.query(query4, function(err, results, fields){
+    let age = parseInt(data['input-age']);
+    if (isNaN(age))
+    {
+        age = 'NULL'
+    }
 
-                    // Send the results to the browser
-                    res.send(JSON.stringify(results));
-                });
-            });
-        });
-    });
-});                                     // requesting the web site.
+    // Create the query and run it on the database
+    query1 = `INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES ('${data['input-fname']}', '${data['input-lname']}', ${homeworld}, ${age})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+            // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/');
+        }
+    })
+})
 
 /*
     LISTENER
